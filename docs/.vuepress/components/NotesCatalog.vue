304eeb90c6bb7643513378630fc8c924cf0e8397
MarkChu-git <markchu2022@gmail.com>
2026-04-04 15:36:43 +0800

    continue fixing

--- docs/.vuepress/components/NotesCatalog.vue
+++ docs/.vuepress/components/NotesCatalog.vue
@@ -244,7 +244,7 @@ const copy = computed(() => {
 })
 
 const visibleCards = computed(() => {
-  return copy.value.cards.filter(card => card.visible === true)
+  return copy.value.cards.filter(card => card.visible !== false)
 })
 </script>
 
